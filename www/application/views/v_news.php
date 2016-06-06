52ae4fbd7cc78cec379cb71c89808001d0898f50
L1verp00ler <andrey.golubev.1991@mail.ru>
2016-06-06 04:42:08 +0300

    Добавление нового функционала + рефакторинг: - реализован вывод ленты новостей (просмотр всех новостей); - реализован просмотр выбранной новости и комментариев к ней.

--- www/application/views/v_news.php
+++ www/application/views/v_news.php
@@ -1,54 +1,10 @@
-﻿<!--
-<link href="/css/signin.css" rel="stylesheet">
-<div class="container">
--->
-
-<?php
-//$result = mysql_query("SELECT * FROM personal") or die(mysql_error());
-//$data = mysql_fetch_array($result);
-?>
-
-<!--
-    <table align="center" border="4" cellspacing="50" cellpadding="50"
-           style="border-color: #4F0889; background-color: #79F1F8; color: #F46512">
-        <tr>
-            <th colspan="3">
-                <h1 align="center">Состав команды ФК Палех на сезон 2015 года</h1>
-            </th>
-        </tr>
--->
-<?
-/*
-do {
-    printf('
-                    <tr>
-                        <td width="200" height="50" valign="middle"  align="center">
-                            <h3> %s %s </h3>
-                        </td>
-                        <td width="200" height="50" valign="middle"  align="center">
-                            <h3> %s  <h3>
-                        </td>
-                        <td width="200" height="50" valign="middle"  align="center">
-                            <h3> %s  <h3>
-                        </td>
-                    </tr>
-                    ', $data["familiya"], $data["name"], $data["position"], $data["stag"]);
-} while ($data = mysql_fetch_array($result));
-*/
-?>
-<!--
-    </table>
-
-</div>
--->
-
-<link href="/css/dashboard.css" rel="stylesheet">
+﻿<link href="/css/dashboard.css" rel="stylesheet">
 <div class="container-fluid">
     <!--div class="starter-template"-->
     <div class="row">
         <div class="col-sm-6 col-md-12 main">
             <br>
-            <h1 class="page-header" style="text-align: center">Состав команды</h1>
+            <h1 class="page-header" style="text-align: center">Новости</h1>
             <? if (isset($_GET['info'])) { ?>
                 <div class="alert alert-success">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
@@ -57,35 +13,66 @@ do {
             <? } ?>
 
             <?php
-            $result1 = mysql_query("SELECT * FROM players AS pl LEFT JOIN positions AS pos
-                                   ON pl.position_id = pos.position_id WHERE pl.position_id = 1") or die(mysql_error());
-            $result2 = mysql_query("SELECT * FROM players AS pl LEFT JOIN positions AS pos
-                                   ON pl.position_id = pos.position_id WHERE pl.position_id = 2") or die(mysql_error());
-            $result3 = mysql_query("SELECT * FROM players AS pl LEFT JOIN positions AS pos
-                                   ON pl.position_id = pos.position_id WHERE pl.position_id = 3") or die(mysql_error());
-            $result4 = mysql_query("SELECT * FROM players AS pl LEFT JOIN positions AS pos
-                                   ON pl.position_id = pos.position_id WHERE pl.position_id = 4") or die(mysql_error());
-            $players = [];
-            $goalkeepers = [];
-            $defenders = [];
-            $midfielders = [];
-            $forwards = [];
-            while ($data1 = mysql_fetch_assoc($result1)) {
-                $players[] = $data1;
-            }
-            while ($data2 = mysql_fetch_assoc($result2)) {
-                $players[] = $data2;
-            }
-            while ($data3 = mysql_fetch_assoc($result3)) {
-                $players[] = $data3;
-            }
-            while ($data4 = mysql_fetch_assoc($result4)) {
-                $players[] = $data4;
-            }
+            if (isset($_GET['id'])) {
+                $query = "SELECT * FROM news WHERE news_id =" . $_GET['id'];
+                $result = mysql_query($query);
+                while ($data = mysql_fetch_assoc($result)) {
+                    $news = $data;
+                }
+                //var_dump($news);
+                ?>
+                <div class="jumbotron">
+                    <div class="row" style="border-bottom: 2px dotted #03498B">
+                        <h2><?php echo $news['title'];?></h2>
+                        <p><?php echo $news['text'];?></p>
+                    </div>
+                    <div class="row">
+                        <h2>Комментарии пользователей:</h2>
+                    </div>
+                    <?php
+                    $query = "SELECT * FROM comments WHERE news_id =" . $_GET['id'];
+                    $result = mysql_query($query);
+                    while ($data = mysql_fetch_assoc($result)) {
+                        $comments[] = $data;
+                    }
+                    foreach ($comments as $comment){ ?>
+                        <div class="row">
+                            <h3><?php echo "From User" . $comment['user_id'] . ":";?></h3>
+                            <p><?php echo $comment['comment_text'];?></p>
+                        </div>
+                        <br>
+                    <?php
+                    }
+                    ?>
+                </div>
+            <?php
+            } else {
+                ?>
 
-            //var_dump($players);
-            //exit;
+                <?php
+                $result = mysql_query("SELECT * FROM news");
+                while ($data = mysql_fetch_assoc($result)) {
+                    $news_array[] = $data;
+                }
+                //var_dump($news);
+                //exit;
+                ?>
+
+                <?
+                foreach ($news_array as $news) {
+                    ?>
+                    <div class="jumbotron">
+                        <div class="row">
+                            <a href="/news?id=<?= $news['news_id'] ?>"><h2><?php echo $news['title']; ?></h2></a>
+                            <p><?php echo $news['text']; ?></p>
+                        </div>
+                    </div>
+                    <?php
+                }
+            }
             ?>
+
+            <!--
             <div class="table-responsive">
                 <table class="table table-striped text-left">
                     <tr>
@@ -97,21 +84,23 @@ do {
                     </tr>
 
                     <?
+                    /*
                     foreach ($players as $player) {
-                        echo "<tr>";
-                        //echo "<td><input id='radio' type='radio' class='radio' name='radio' value='" . $personal['id'] . "'>
-                        //		  <input id='name2' type='hidden' name='name2' value='" . $personal['name'] . " " . $personal['familiya'] . "'></td> ";
-                        echo "<td class = 'tdm'> " . $player['player_id'] . " </td>";
-                        echo "<td class = 'tdm'> " . $player['lastname'] . " </td>";
-                        echo "<td class = 'tdm'> " . $player['firstname'] . " </td>";
-                        echo "<td class = 'tdm'> " . $player['position_name'] . " </td>";
-                        echo "<td class = 'tdm'> " . $player['age'] . " </td>";
-                        echo "</tr>";
+                    echo "<tr>";
+                    //echo "<td><input id='radio' type='radio' class='radio' name='radio' value='" . $personal['id'] . "'>
+                    //		  <input id='name2' type='hidden' name='name2' value='" . $personal['name'] . " " . $personal['familiya'] . "'></td> ";
+                    echo "<td class = 'tdm'> " . $player['player_id'] . " </td>";
+                    echo "<td class = 'tdm'> " . $player['lastname'] . " </td>";
+                    echo "<td class = 'tdm'> " . $player['firstname'] . " </td>";
+                    echo "<td class = 'tdm'> " . $player['position_name'] . " </td>";
+                    echo "<td class = 'tdm'> " . $player['age'] . " </td>";
+                    echo "</tr>";
                     }
+                    */
                     ?>
                 </table>
-                </form>
             </div>
+            -->
         </div>
     </div> <!-- endrow-->
     <!--/div-->
